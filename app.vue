01046d874dde6bb1dbb527df656e4853566da71a
Michael.Kryvoruchko <Michael.Kryvoruchko@mdm.de>
2022-03-02 15:11:35 +0200

    init

---
+++ app.vue
@@ -0,0 +1,43 @@
+<template>
+  <main class="main">
+    <div class="list">
+      <a href="#" class="item">
+        Good
+      </a>
+      <a href="#" class="item">
+        Jobs
+      </a>
+      <a href="#" class="item">
+        About
+      </a>
+    </div>
+  </main>
+</template>
+
+<style lang="postcss" scoped>
+.main {
+  @apply p-4;
+}
+
+.list {
+  @apply
+    py-2
+    flex justify-center
+  ;
+}
+
+.item {
+  @apply
+    text-xs text-gray-500
+    px-2
+
+    hover:(text-gray-700)
+  ;
+
+  &:not(:last-child) {
+    @apply
+     border-gray-500 border-r-1
+    ;
+  }
+}
+</style>
